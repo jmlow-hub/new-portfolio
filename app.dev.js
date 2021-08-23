d66564ac46caba29e89ce31b898d015645532979
Jackie <jmlow74@gmail.com>
2021-08-23 11:11:05 +0100

    Styling update, new links added

--- app.dev.js
+++ app.dev.js
@@ -3,36 +3,43 @@
 //grab DOM elements for nav menu
 var openMenuBtn = document.querySelector(".nav__open");
 var closeMenuBtn = document.querySelector(".nav__close");
-var navMenu = document.querySelector(".nav"); //variables for typing effect
+var navMenu = document.querySelector(".nav");
+var ctaElement = document.querySelector(".title__CTA"); //variables for typing effect
 
 var heading = document.querySelector("h1");
-var headingText = "//Jackie Low Software Developer"; //text for heading
+var headingText = "//Jackie Low"; //text for heading
 
 var testBtn = document.getElementById("test");
 var i = 0; //index for if statement - needs to be global else will loop
+//function to set visibility of cta:
+// const ctaVisibility = () => {
+//   ctaElement.classList.add("visible") 
+//   setTimeout(ctaVisibility, 1000)
+//   }
 //typing function:
-
-var typewriter = function typewriter() {
-  if (i < headingText.length) {
-    heading.innerHTML += headingText.charAt(i);
-    i++;
-  }
-
-  setTimeout(typewriter, 75); //calls the function with delay of 75ms until i is greater than the text length
-};
-
-window.addEventListener("DOMContentLoaded", function () {
-  typewriter();
-}); //function to open nav menu:
+// const typewriter = () => {  
+//   if(i < headingText.length) {
+//     heading.innerHTML += headingText.charAt(i);
+//     i++;       
+//  }
+//  setTimeout(typewriter, 75)//calls the function with delay of 75ms until i is greater than the text length
+// } 
+// const display = window.addEventListener("DOMContentLoaded", () => {
+//   typewriter()
+//   //ctaVisibility()
+// })
+//function to open nav menu:
 
 var handleNavOpen = openMenuBtn.addEventListener("click", function (e) {
-  navMenu.style.width = "200px";
-  openMenuBtn.style.display = "none";
-  closeMenuBtn.style.display = "block";
+  navMenu.classList.add("showMenu");
+  document.body.style.overflow = "hidden"; // navMenu.style.width = "200px";
+  // openMenuBtn.style.display = "none";
+  // closeMenuBtn.style.display = "block";
 }); //function to close nav menu:
 
 var handleNavClose = closeMenuBtn.addEventListener("click", function (e) {
-  navMenu.style.width = "0";
-  closeMenuBtn.style.display = "none";
-  openMenuBtn.style.display = "block";
+  navMenu.classList.remove("showMenu");
+  document.body.style.overflow = "auto"; // navMenu.style.width = "0";
+  // closeMenuBtn.style.display="none";
+  // openMenuBtn.style.display = "block";
 });
--- app.js
+++ app.js
@@ -3,25 +3,7 @@ const openMenuBtn = document.querySelector(".nav__open");
 const closeMenuBtn = document.querySelector(".nav__close");
 const navMenu = document.querySelector(".nav");
 
-//variables for typing effect
-const heading = document.querySelector("h1");
-const headingText = "//Jackie Low Software Developer" //text for heading
-const testBtn = document.getElementById("test");
-let i = 0; //index for if statement - needs to be global else will loop
-//typing function:
-const typewriter = () => {  
-  if(i < headingText.length) {
-    heading.innerHTML += headingText.charAt(i);
-    i++;       
- }
- setTimeout(typewriter, 75)//calls the function with delay of 75ms until i is greater than the text length
-  
-  } 
 
-window.addEventListener("DOMContentLoaded", () => {
-  typewriter()
- 
-})
 
 
 
@@ -29,14 +11,17 @@ window.addEventListener("DOMContentLoaded", () => {
 
 //function to open nav menu:
 const handleNavOpen = openMenuBtn.addEventListener("click", e => {
-  navMenu.style.width = "200px";
-  openMenuBtn.style.display = "none";
-  closeMenuBtn.style.display = "block";
+    navMenu.classList.add("showMenu");
+
+    document.body.style.overflow = "hidden";
+
 })
 
 //function to close nav menu:
 const handleNavClose = closeMenuBtn.addEventListener("click", e => {
-  navMenu.style.width = "0";
-  closeMenuBtn.style.display="none";
-  openMenuBtn.style.display = "block";
+
+    navMenu.classList.remove("showMenu");
+
+    document.body.style.overflow = "auto";
+ 
 } )
